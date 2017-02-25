40b03d47a94c18b518223d844d55b4f9d0ad74d1
Aung Khant <theaungkhant@gmail.com>
2017-02-25 17:35:53 +0000

    Read Update Delete Cats

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="catApp">
+    <div id="catApp" class="center">
         <h2> {{ title }} </h2>
         <h4>  New Cat <h4>
 
@@ -20,6 +20,28 @@
             <button v-if="add">Add</button>
             <button v-else>Add</button>
         </form>
+
+
+         <h4> All Cat Lists</h4>
+            <table>
+                <tr>
+                    <th> Name </th>
+                    <th> Age </th>
+                    <th> Owner </th>
+                    <th> Pregnant </th>
+                    <th> Update </th>
+                    <th> Delete </th>
+                </tr>
+                <tr v-for="(c, index) in cats">
+                    <td>{{ c.name }}</td>
+                    <td>{{ c.age }}</td>
+                    <td>{{ c.owner }}</td>
+                    <td v-if="c.pregnant">✓</td>
+                    <td v-else>✗</td>
+                    <td v-on:click.prevent="onEdit(index)"><a>Edit</a></td>
+                    <td v-on:click.prevent="onDelete(index)"><a>Delete</a></td>
+                </tr>
+            </table>
     </div>
 </template>
 
@@ -55,6 +77,27 @@
                         owner: '',
                         pregnant: false
                     }
+                },
+
+                onEdit(index) {
+                    this.updating = true;
+                    this.updateIndex = index;
+                    this.cat = this.cats[index];
+                },
+
+                onUpdate() {
+                    this.updating = false;
+                    this.cats[this.updateIndex] = this.cat;
+                     this.cat = {
+                        'name': '',
+                        'age': '',
+                        'owner': '',
+                        'pregnant': false
+                    }
+                },
+
+                onDelete(index){
+                    this.cats.splice(index, 1)
                 }
             }
         }
@@ -72,5 +115,18 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+table {
+    border-collapse: collapse;
+}
+
+table, th, td {
+    border: 1px solid black;
+}
+.center {
+    margin: auto;
+    width: 50%;
+    border: 3px solid green;
+    padding: 10px;
 }
 </style>
